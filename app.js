7fbef1b476264224d42970eb12b913668ef36bdf
Kevoard <kevoard.mined@gmail.com>
2024-06-11 19:28:02 +0100

    lesson2

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3025
+const express = require("express");
+const app = express();
+const port = 3025;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname})
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+
+
+mongoose
+  .connect(
+    "mongodb+srv://kevoardmined:wiiTm45xbloPk4PH@kevmind.oo8jy0t.mongodb.net/all-data?retryWrites=true&w=majority&appName=KevMind"
+  )
+  .then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
